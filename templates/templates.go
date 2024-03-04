2cb4fdaedbec1ec18e357682d2839b3262e033fc
Filip Solich <dev@solich.me>
2024-03-04 20:54:55 +0100

    Add add repo modal basics

--- templates/templates.go
+++ templates/templates.go
@@ -5,13 +5,14 @@ import (
 	"html/template"
 )
 
-//go:embed *.html base/*.html partials/*.html
+//go:embed *.html base/*.html partials/*.html errors/*.html
 var templates embed.FS
 
 var (
 	IndexTmpl = template.Must(template.New("base.html").Funcs(FuncMap).ParseFS(templates, "base/base.html", "base/layout.html", "index.html", "partials/repo.html"))
 	LoginTmpl = template.Must(template.New("base.html").Funcs(FuncMap).ParseFS(templates, "base/base.html", "login.html"))
 
+	Error404Tmpl = template.Must(template.New("base.html").Funcs(FuncMap).ParseFS(templates, "base/base.html", "errors/404.html"))
 	Error5xxTmpl = template.Must(template.New("base.html").Funcs(FuncMap).ParseFS(templates, "base/base.html", "errors/5xx.html"))
 )
 
